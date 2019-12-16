10ac87f66db0854efd2a4b0cf27e2a3b01330c93
Titoo <title.spp@gmail.com>
2019-12-17 00:22:04 +0700

    fix codeCheck

--- src/pages/fourelements/utils/codeCheck.js
+++ src/pages/fourelements/utils/codeCheck.js
@@ -5,7 +5,7 @@ export function codeCheckRound1(players, group) {
   group.map((player, i) => {
     //ดักว่าเกิน 3 คน
     if (i > 2) {
-      break;
+      return;
     }
     const element = player.element;
     if (!elements[element]) {
@@ -14,31 +14,24 @@ export function codeCheckRound1(players, group) {
     elements[element].push(player.uid);
   });
   const allElements = Object.keys(elements);
-  switch (allElements.length) {
-    case 1: {
-      const elementSame = allElements[0];
-      newPlayers = newPlayers.map(player => {
-        if (elements[elementSame].includes(player.uid)) {
-          return { ...player, money: player.money + 1000 };
-        }
-        return player;
-      });
-    }
-    case 2: {
-      const elementSame =
-        elements[allElements[0]].length === 1 ? allElements[1] : allElements[0];
-      const elementDiff =
-        elementSame === allElements[1] ? allElements[0] : allElements[1];
-      newPlayers = newPlayers.map(player => {
-        if (elements[elementSame].includes(player.uid)) {
-          return { ...player, money: player.money - 1000 };
-        } else if (elements[elementDiff].includes(player.uid)) {
-          return { ...player, money: player.money + 2000 };
-        }
-        return player;
-      });
-    }
+  if (allElements.length === 1) {
+    const elementSame = allElements[0];
+    elements[elementSame].map(uid => {
+      newPlayers[uid].money += 1000;
+    });
+  } else if (allElements.length === 2) {
+    const elementSame =
+      elements[allElements[0]].length === 1 ? allElements[1] : allElements[0];
+    const elementDiff =
+      elementSame === allElements[1] ? allElements[0] : allElements[1];
+    elements[elementSame].map(uid => {
+      newPlayers[uid].money -= 1000;
+    });
+    elements[elementDiff].map(uid => {
+      newPlayers[uid].money += 2000;
+    });
   }
+  return newPlayers;
 }
 
 export function codeCheckRound2(players, group) {
